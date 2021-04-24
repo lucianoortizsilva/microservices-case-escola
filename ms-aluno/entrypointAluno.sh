2ceca2db9d88341cb2afabbae097eb854b8c87f7
lucianoortizsilva <lucianoortizsilva@gmail.com>
2021-04-23 23:12:43 -0300

    Add esqueleto do ms-professor

--- ms-aluno/entrypointAluno.sh
+++ ms-aluno/entrypointAluno.sh
@@ -4,7 +4,7 @@ while ! nc -z eureka-server-app 8761 ; do
     echo "##########################################"
     echo "######## Aguardando Eureka-server ########"
     echo "##########################################"
-    sleep 30
+    sleep 20
 done
 
 java -jar -Dspring.profiles.active=${ENV} /opt/lib/ms-aluno.jar
--- ms-professor/entrypointProfessor.sh
+++ ms-professor/entrypointProfessor.sh
@@ -4,7 +4,7 @@ while ! nc -z eureka-server-app 8761 ; do
     echo "##########################################"
     echo "######## Aguardando Eureka-server ########"
     echo "##########################################"
-    sleep 30
+    sleep 40
 done
 
-java -jar -Dspring.profiles.active=${ENV} /opt/lib/ms-boletim.jar
+java -jar -Dspring.profiles.active=${ENV} /opt/lib/ms-professor.jar
